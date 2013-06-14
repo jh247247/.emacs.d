b8cd9ecd6311976b968bbeb255d711b9f5e307ae
Jack Hosemans <jh247247@gmail.com>
2013-06-14 18:14:57 +1000

    Changed back to old theme, low-contrast is annoying. Modified rainbow-delimiters to be on a grayscale instead of rainbow.

--- appearance.el
+++ appearance.el
@@ -38,9 +38,10 @@
 ;; go back to old, comfortable theme...
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-sanityinc-tomorrow-night)
+(color-theme-billw)
+;;(color-theme-sanityinc-tomorrow-night)
 ;;(color-theme-monokai)
-;; (color-theme-billw)
+
 
 
 ;; highlight uneeded whitespace and columns past char 80
